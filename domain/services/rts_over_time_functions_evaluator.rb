87ab042a810a2bc87aee7cc1545a06f187e0e4b1
Jacob <jacob.mendoza@gmail.com>
2018-10-02 21:31:39 +0200

    Removing Rails code

--- domain/services/rts_over_time_functions_evaluator.rb
+++
@@ -1,6 +0,0 @@
-# Evaluates all the classes provided for an specific t
-class RtsOverTimeFunctionsEvaluator
-  def evaluate(fns, t)
-    fns.map { |f| f[t] unless f[t].nil? }.compact
-  end
-end
--- domain/spec/builders/api_tweet_user_builder.rb
+++
@@ -1,5 +0,0 @@
-# Helper for building twitter users for the tests
-class ApiTweetUserBuilder
-  attr_accessor :id, :name, :screen_name, :location, :description,
-                :profile_background_image_url, :profile_image_url
-end
